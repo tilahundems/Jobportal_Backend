9cbea5a09f2dbae150ca73118af20170eb333a1c
Tilahun Demsis Keraga <dtilahun@abaybank.local>
2025-09-05 16:34:25 +0300

    deploy  to  Railway now

--- obj/Debug/net8.0/JobPortalAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/JobPortalAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobPortalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cec4f48ce450a2f632ec2686f3d0f8c7568ec4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobPortalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobPortalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
